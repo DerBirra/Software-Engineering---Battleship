a8b94b44ddeef2a381fdda90cdb32d9c2d055923
MikBarut <mikail.barut01@gmail.com>
2024-05-09 23:55:54 +0200

    Codecoverage100%

--- src/test/scala/model/BattleshipSpec.scala
+++ src/test/scala/model/BattleshipSpec.scala
@@ -6,45 +6,37 @@ import model.GameField
 import scala.annotation.meta.field
 
 class BattleshipSpec extends AnyWordSpec{
-
-    "Battleship" should{
-        "Ships" in{
-            val ship1 = Battleship(Ship.Carrier, Shipsize.Five, Shipindex.Two)
+    val ship1 = Battleship(Ship.Carrier, Shipsize.Five, Shipindex.Two)
+    val ship2 = Battleship(Ship.Battleship, Shipsize.Five, Shipindex.Two)
+    val ship3 = model.Battleship(Ship.Destroyer, Shipsize.Four, Shipindex.Two)
+    val ship4 = model.Battleship(Ship.Submarine, Shipsize.Three, Shipindex.Two)
+    val ship5 = model.Battleship(Ship.PatrolBoat, Shipsize.Two, Shipindex.Two)
+    
+    "Battleshiptype" should{
+        "initialized correct" in{
+            ship1.stype shouldEqual Ship.Carrier
+            ship2.stype shouldEqual Ship.Battleship
+            ship3.stype shouldEqual Ship.Destroyer
+            ship4.stype shouldEqual Ship.Submarine
+            ship5.stype shouldEqual Ship.PatrolBoat
+        }
+    }
+    "Shipsize" should{
+        "initialized correct" in{
             ship1.size shouldEqual Shipsize.Five
-            val ship2 = Battleship(Ship.Destroyer, Shipsize.Three, Shipindex.Two)
-            ship2.stype shouldEqual Ship.Destroyer
-            val ship3 = model.Battleship(Ship.Carrier, Shipsize.Three, Shipindex.Two)
+            ship2.size shouldEqual Shipsize.Five
+            ship3.size shouldEqual Shipsize.Four
+            ship4.size shouldEqual Shipsize.Three
+            ship5.size shouldEqual Shipsize.Two
+        }
+    }
+    "Shipindex" should{
+        "initialized correct" in{
+            ship1.index shouldEqual Shipindex.Two
+            ship2.index shouldEqual Shipindex.Two
             ship3.index shouldEqual Shipindex.Two
+            ship4.index shouldEqual Shipindex.Two
+            ship5.index shouldEqual Shipindex.Two
         }
-        "Field" in{
-         val gameField = new GameField(9,9)
-         val testField = new GameField(9,9)
-         gameField.generateField()
-         testField.generateField()
-         /*val expectedField = 
-            """+---------------------------+
-| #  #  #  #  #  #  #  #  # |
-| #  #  #  #  #  #  #  #  # |
-| #  #  #  #  #  #  #  #  # |
-| #  #  #  #  #  #  #  #  # |
-| #  #  #  #  #  #  #  #  # |
-| #  #  #  #  #  #  #  #  # |
-| #  #  #  #  #  #  #  #  # |
-| #  #  #  #  #  #  #  #  # |
-| #  #  #  #  #  #  #  #  # |
-+---------------------------+"""*/
-         gameField.printField() shouldEqual testField.printField()
-        }
-    }
-
-    "Player"in{
-        val player = Player("Miki")
-        player.name shouldEqual "Miki"
-    }
-    "Cell" in{
-        val field = new GameField(9,9)
-        field.generateField()
-        field.cell(1,1,Some('x')) shouldEqual Some('x')
-        field.isCellContent(1,1,'x') shouldEqual true
     }
 }
