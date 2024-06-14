902fa41864b2ba8f90c1052dec86cc0eaa962cbe
KayaFirat <kevinfirat7@gmail.com>
2024-06-14 03:34:32 +0200

    Added gamField

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -12,29 +12,45 @@ object Main {
  
   def main(args: Array[String]): Unit = {
 
-    println(s"$GREEN" +"Geben sie die Feldgröße an")
+    //println(s"$GREEN" +"Geben sie die Feldgröße an")
     var size = readInt()
 
-    val gameBoard1 = new GameBoard(size)
-    val gameBoard2 = new GameBoard(size)
-    val controller = new Controller(gameBoard1, gameBoard2)
-    val tui1 = new TUI(controller,1)
-    val gui = new GUI(controller)
-
-    controller.add(tui1)
-    controller.add(gui)
-
-    gui.initialize()
+    var gameBoard1 = new GameBoard(size)
+    var gameBoard2 = new GameBoard(size)
+    var controller = new Controller(gameBoard1, gameBoard2)
+    
+    initializeGUI(controller)
+    initializeTUI(controller)
     
     
+
+    
+  }
+
+  def initializeTUI(controller: Controller): Unit = {
+
+    val tui = new TUI(controller, 1)
+    controller.add(tui)
+
 
     println(s"$GREEN" +"Geben sie 'start' oder 'exit' ein." + s"$RESET")
 
     var input: String = ""
     input = readLine()
 
-    tui1.processInput(input)
+    tui.processInput(input)
 
-    
+
+
+  }
+
+  def initializeGUI(controller: Controller): Unit = {
+
+    val gui = new GUI(controller)
+    controller.add(gui)
+
+    gui.initialize()
+
+
   }
 }
