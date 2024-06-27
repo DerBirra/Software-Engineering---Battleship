1066f582a4ed3443167ac801ebd28c410deb4bff
KayaFirat <kevinfirat7@gmail.com>
2024-06-27 17:44:42 +0200

    added FileIO

--- src/main/scala/FileIOModule.scala
+++ src/main/scala/FileIOModule.scala
@@ -3,7 +3,7 @@ import com.google.inject.name.Names
 
 class FileIOModule extends AbstractModule {
   override def configure(): Unit = {
-    bind(classOf[FileIO]).annotatedWith(Names.named("xml")).to(classOf[XMLFileIO])
-    bind(classOf[FileIO]).annotatedWith(Names.named("json")).to(classOf[JSONFileIO])
+    bind(classOf[FileIO]).annotatedWith(Names.named("xml")).to(classOf[XML])
+    bind(classOf[FileIO]).annotatedWith(Names.named("json")).to(classOf[JSON])
   }
 }
